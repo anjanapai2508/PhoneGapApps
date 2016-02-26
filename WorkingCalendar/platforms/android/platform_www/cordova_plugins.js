8b336b1faa317e2e8e7c4999d5b33829000ec7fd
anjanapai2508 <PaiA@MXL53811KX.C-IV.org>
2016-02-25 17:43:04 -0800

    Initial Commit for Calendar Plugin

---
+++ WorkingCalendar/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-calendarplugin/www/calendar.js",
+        "id": "cordova-plugin-calendarplugin.CalendarPlugin"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
